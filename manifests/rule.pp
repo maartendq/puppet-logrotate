e5016902d265e70d7d5428bf1972cc788b01bcbb
maartendq <mde_quick@hotmail.com>
2013-07-09 12:48:21 +0200

    Adding rule manifest for defining logrotate rule

---
+++ manifests/rule.pp
@@ -0,0 +1,91 @@
+# == Define: up_logrotate::rule
+#
+# Defines a logrotation schedule for a log file.
+#
+# === Parameters
+#
+# Full documentation of each parameter's meaning
+# can be found at http://linux.die.net/man/8/logrotate .
+#
+# log
+#
+# interval
+#
+# rotation
+#
+# size
+#
+# maxage
+#
+# minsize
+#
+# options
+#
+# archive
+#
+# olddir
+#
+# olddir_owner
+#
+# olddir_group
+#
+# olddir_mode
+#
+# create
+#
+# postrotate
+#
+# === Examples
+#
+#   logrotate::rule { 'foo':
+#     log         => '/var/log/foo.log',
+#     interval    => 'daily',
+#      rotation   => '90',
+#      options    => [
+#        'missingok',
+#        'notifempty',
+#        'sharedscripts',
+#      ]
+#      postrotate => 'Your command here',
+#   }
+#
+define up_logrotate::rule (
+  $log          = false,
+  $interval     = false,
+  $rotation     = false,
+  $size         = false,
+  $maxage       = false,
+  $minsize      = false,
+  $options      = false,
+  $archive      = false,
+  $olddir       = 'UNSET',
+  $olddir_owner = 'um',
+  $olddir_group = 'um',
+  $olddir_mode  = '0755',
+  $create       = false,
+  $postrotate   = false
+) inherits logrotate {
+
+  $olddir_real = $olddir ? {
+    'UNSET'   => "${logrotate::logrotate_archive_dir}/${name}",
+    default   => $olddir,
+  }
+
+  file { $olddir_real:
+    ensure  => 'directory',
+    owner   => $olddir_owner,
+    group   => $olddir_group,
+    mode    => $olddir_mode,
+    require => File[$logrotate::logrotate_archive_dir],
+  }
+
+  file { "/etc/logrotate.d/${name}":
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/logrotate_rule.erb"),
+    require => File['/etc/logrotate.d'],
+  }
+
+}
